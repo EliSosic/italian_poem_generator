5456394d7f1a24dabbd8baee90876fcc7990f478
Elisa <elisosic@gmail.com>
2024-01-27 13:31:34 +0100

    improved the AI prompt

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function writePoem (event) {
 
    let apiKey = "3c836392eab01t60930bboe42ecabfe4";
    let context = "You are a smart italian AI assistent who knows a lot of poems. You are also very good at writing short romantic italian poems.";
-   let prompt = `write a very short poem, maximum 5 sentences, in italian about ${topicInput.value}. Use basic html in italic to display it and separate each sentence with </br>. Sign the poem with Il tuo Amore`;
+   let prompt = `write a very short poem, maximum 5 sentences, in italian about ${topicInput.value}. Use basic html in italic to display it and separate each sentence going in a new line with < /br>. At the bottom of the poem sign it with Il tuo Amore`;
    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
     //let poemOutput = docmunet.querySelector("#poem");
